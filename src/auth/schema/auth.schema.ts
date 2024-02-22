d3db64b68b302afa2cb887324705d2a7f47f1e46
alinetixsol <ali_muhammad_intern@netixsol.com>
2024-02-22 11:33:44 +0500

    SecondCommit

--- src/auth/schema/auth.schema.ts
+++ src/auth/schema/auth.schema.ts
@@ -1,21 +1,18 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 @Schema({
-    timestamps: true
+  timestamps: true,
 })
-
 export class User extends Document {
+  @Prop({ required: true })
+  name: string;
 
-    @Prop({required: true})
-    name: string;
+  @Prop({ required: true, unique: true, message: 'Duplicate Email!' })
+  email: string;
 
-    @Prop({required: true, unique: true, message: 'Duplicate Email!'})
-    email: string;
-
-    @Prop({required: true})
-    password: string;
-
+  @Prop({ required: true })
+  password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
